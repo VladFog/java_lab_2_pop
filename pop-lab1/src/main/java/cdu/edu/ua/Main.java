13bd36eb3b2a3b95bcffe70ebf86f978047a74ba
VladFog <vgulida341@gmail.com>
2023-06-22 06:20:07 +0300

    lab_2

---
+++ pop-lab1/src/main/java/cdu/edu/ua/Main.java
@@ -0,0 +1,11 @@
+package cdu.edu.ua;
+
+public class Main {
+    public static void main(String[] args) {
+        int threadCount = 10;
+        BreakThread breakThread = new BreakThread(threadCount);
+        ThreadController threadController = new ThreadController(threadCount, breakThread);
+
+        new Thread(breakThread).start();
+    }
+}
